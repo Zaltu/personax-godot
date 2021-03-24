d06dfd64e13ac2c084856deb827dcd04e0dd2ddd
Zaltu <swwouf@hotmail.com>
2021-03-23 22:24:15 -0400

    working battle env start

---
+++ PersonaX/envs/battle/BattleCamera.gd
@@ -0,0 +1,64 @@
+extends Camera
+
+# Based on
+# https://github.com/godot-extended-libraries/godot-interpolated-camera3d
+# under MIT license
+
+# Offset to the camera relative to the target position
+const PULL_BACK = Vector3(0, 30, 20)
+
+# Desired camera look_at for rotation
+var looking_at_target = Vector3(0, 0, 0)
+
+# The factor to use for asymptotical translation lerping.
+# If 0, the camera will stop moving. If 1, the camera will move instantly.
+var translate_speed := 0.1
+
+# The factor to use for asymptotical rotation lerping.
+# If 0, the camera will stop rotating. If 1, the camera will rotate instantly.
+var rotate_speed := 0.1
+
+# The node to target.
+var target: NodePath
+
+# Enable/disable interpolation
+var enabled = false
+
+func set_interpolation_enabled(enable):
+	enabled = enable
+
+
+func sift_over_to(bullseye, lookat):
+	# bullseye is target to move to
+	# lookat is location to look at, usually, another target's transform.
+	target = bullseye.get_path()
+	looking_at_target = lookat
+
+
+func set_speed(multiplier):
+	# Should be a number between 0 and 1.
+	rotate_speed = multiplier
+	translate_speed = multiplier
+
+
+func _process(delta: float) -> void:
+	if not enabled:
+		return
+	if not has_node(target):
+		return
+
+	# TODO: Fix delta calculation so it behaves correctly if the speed is set to 1.0.
+	var translate_factor = translate_speed * delta * 10
+	var rotate_factor = rotate_speed * delta * 10
+	var target_node = get_node(target) as Node
+	var target_xform = target_node.get_global_transform()
+	# Adjust to BattleCamera specs
+	target_xform = target_xform.translated(PULL_BACK)
+	target_xform = target_xform.looking_at(looking_at_target, Vector3(0, 1, 0))
+	# Interpolate the origin and basis separately so we can have different translation and rotation
+	# interpolation speeds.
+	var local_transform_only_origin := Transform(Basis(), get_global_transform().origin)
+	var local_transform_only_basis := Transform(get_global_transform().basis, Vector3())
+	local_transform_only_origin = local_transform_only_origin.interpolate_with(target_xform, translate_factor)
+	local_transform_only_basis = local_transform_only_basis.interpolate_with(target_xform, rotate_factor)
+	set_global_transform(Transform(local_transform_only_basis.basis, local_transform_only_origin.origin))
--- PersonaX/envs/battle/demoBattle.gd
+++ PersonaX/envs/battle/demoBattle.gd
@@ -2,18 +2,19 @@ extends Spatial
 
 # Spacers should be calculated dynamically, TODO
 # Char name, char name
-const PARTY_RES_PATH = "res://assets/3d/chars/{0}/{1}.tscn"
-const PARTY_Z_OFFSET = 15
+const PARTY_RES_PATH = "res://assets/3d/chars/{0}/{1}.glb"
+const PARTY_Z_OFFSET = 10
 const PARTY_SPACER = 20
 # Persona arcana, persona name
 const PERSONA_RES_PATH = "res://assets/3d/personas/{0}/{1}/{2}.glb"
 # Enemy name, enemy name (TODO)
 #const ENEMY_RES_PATH = "res://assets/3d/enemies/{0}/{1}.tscn"
-const ENEMY_Z_OFFSET = -15
+const ENEMY_Z_OFFSET = -10
 const ENEMY_SPACER = 20
 
 onready var party_models = []
 onready var enemy_models = []
+onready var cam = get_node("BattleCamera")
 
 func _ready():
 	# We assume that the GSV is already in the battle context when Godot is
@@ -23,34 +24,38 @@ func _ready():
 	var participants = update["participants"]
 	var partylen = len(update["iparty"])
 	var enemylen = len(update["ienemy"])
+	var target
 	for ially in update["iparty"]:
 		# -1 cause Lua omegalul
 		spawn_participant(update["participants"][ially-1], partylen, true)
+		if ially == update["open"]:  # Unadjusted indexes
+			target = party_models[-1]
 	for ienemy in update["ienemy"]:
 		# -1 cause Lua omegalul
 		spawn_participant(update["participants"][ienemy-1], enemylen, false)
 	
-	activate_camera()
+	activate_camera(target)
 	process_turns(update)
 
-func spawn_participant(participant, listlength, partymember):
+func spawn_participant(participant, listlength, ispartymember):
 	# A bit messy since the "real" enemies won't be in the persona list.
 	# This will need to be cleaned up when the real enemy resource structure
 	# is available
-	if partymember:
+	if ispartymember:
 		print("Loading " + PARTY_RES_PATH.format([participant["name"], participant["name"]]))
-		party_models.append(load(
+		var partymodel = load(
 			PARTY_RES_PATH.format([
-				participant["name"],
-				participant["name"]
+				participant["name"].to_lower(),
+				participant["name"].to_lower()
 			])
-		).instance())
-		add_child(party_models[-1])
-		var xoffset = (((listlength-1)*PARTY_SPACER)/2)-(PARTY_SPACER*(len(party_models)-1))
-		party_models[-1].translate(Vector3(xoffset, 0, PARTY_Z_OFFSET))
-		party_models[-1].scale_object_local(Vector3(0.5, 0.5, 0.5))
-		party_models[-1].look_at(Vector3(0, 0, 0), Vector3(0, 1, 0))
-		party_models[-1].rotate_object_local(Vector3(0, 1, 0), PI)
+		).instance()
+		add_child(partymodel)
+		var xoffset = (((listlength-1)*PARTY_SPACER)/2)-(PARTY_SPACER*(len(party_models)))
+		party_models.append((partymodel))
+		partymodel.translate(Vector3(xoffset, 0, PARTY_Z_OFFSET))
+		partymodel.scale_object_local(Vector3(0.33, 0.33, 0.33))
+		partymodel.look_at(Vector3(0, 0, ENEMY_Z_OFFSET), Vector3(0, 1, 0))
+		#partymodel.rotate_object_local(Vector3(0, 1, 0), PI)
 	else:
 		print("Loading " + PERSONA_RES_PATH.format([
 				participant["persona"]["arcana"],
@@ -64,15 +69,20 @@ func spawn_participant(participant, listlength, partymember):
 				participant["name"].to_lower()
 			])
 		).instance()
-		enemy_models.append(enemymodel)
 		add_child(enemymodel)
-		var xoffset = (((listlength-1)*ENEMY_SPACER)/2)-(ENEMY_SPACER*(len(enemy_models)-1))
+		var xoffset = (((listlength-1)*ENEMY_SPACER)/2)-(ENEMY_SPACER*(len(enemy_models)))
+		enemy_models.append(enemymodel)
 		enemymodel.translate(Vector3(xoffset, 0, ENEMY_Z_OFFSET))
-		enemymodel.scale_object_local(Vector3(4, 4, 4))
+		enemymodel.scale_object_local(Vector3(3, 3, 3))
+		enemymodel.look_at(Vector3(0, 0, PARTY_Z_OFFSET), Vector3(0, 1, 0))
+		#enemymodel.rotate_object_local(Vector3(0, 1, 0), PI)
 
 
-func activate_camera():
-	get_node("InterpolatedCamera").set_visible(true)
+func activate_camera(target):
+	cam.set_visible(true)
+	cam.sift_over_to(target, Vector3(0, 0, ENEMY_Z_OFFSET))
+	cam.set_interpolation_enabled(true)
+	
 
 func process_turns(full_update):
 	pass
